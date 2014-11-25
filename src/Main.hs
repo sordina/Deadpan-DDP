6dcf2724664f99790d00c1896425cac88747b51b
Lyndon Maydwell <lyndon@sordina.net>
2014-11-26 01:21:58 +1100

    Improved error reporting in debugging app

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,9 @@
 module Main (main) where
 
-import System.Environment
+import System.IO
+import System.Exit
 import Web.DDP.Deadpan
+import System.Environment
 
 main :: IO ()
 main = getArgs >>= go
@@ -9,10 +11,10 @@ main = getArgs >>= go
 go :: [String] -> IO ()
 go xs | hashelp xs = help
 go [url]           = run $ getURI url
-go _               = help
+go _               = help >> exitFailure
 
 run :: Either Error Params -> IO ()
-run (Left  err   ) = print err
+run (Left  err   ) = hPutStrLn stderr err >> exitFailure
 run (Right params) = do logger <- loggingClient
                         runClient logger params (liftIO $ void getLine)
 
@@ -20,4 +22,4 @@ hashelp :: [String] -> Bool
 hashelp xs = any (flip elem xs) (words "-h --help")
 
 help :: IO ()
-help = putStrLn "Usage: deadpan [-h | --help] <URL>"
+help = hPutStrLn stderr "Usage: deadpan [-h | --help] <URL>"
